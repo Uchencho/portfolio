703befd3792b1245f59dde2144a911db731eeb13
Uchencho <aloziekelechi17@gmail.com>
2021-04-10 22:29:13 +0100

    icons done

--- lib/screens/const.dart
+++ lib/screens/const.dart
@@ -5,3 +5,8 @@ const kUcheTextStyle =
 
 const kAlozieTextStyle =
     TextStyle(color: Colors.yellow, fontSize: 80, fontWeight: FontWeight.bold);
+
+const kRoleDescription =
+    TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w300);
+
+const kIconSize = 25.0;
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:portfolio/screens/const.dart';
 
 class Home extends StatelessWidget {
@@ -7,19 +8,39 @@ class Home extends StatelessWidget {
     return Scaffold(
       body: Container(
         alignment: Alignment.center,
-        child: Row(
-          children: [
-            Text(
-              "  Uchenna",
-              style: kUcheTextStyle,
-            ),
-            Expanded(
-              child: Text(
-                " Alozie",
-                style: kAlozieTextStyle,
+        child: Container(
+          padding: EdgeInsets.only(left: 30.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                children: [
+                  Text("Uchenna", style: kUcheTextStyle),
+                  Expanded(child: Text(" Alozie", style: kAlozieTextStyle))
+                ],
               ),
-            )
-          ],
+              SizedBox(height: 10),
+              Text("Software craftman", style: kRoleDescription),
+              SizedBox(height: 20),
+              Container(
+                width: 140,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Icon(FontAwesomeIcons.twitter,
+                        color: Colors.white, size: kIconSize),
+                    Icon(FontAwesomeIcons.linkedin,
+                        color: Colors.white, size: kIconSize),
+                    Icon(FontAwesomeIcons.github,
+                        color: Colors.white, size: kIconSize),
+                    Icon(FontAwesomeIcons.envelope,
+                        color: Colors.white, size: kIconSize)
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
         decoration: BoxDecoration(
           color: Color(0xFF686868),
