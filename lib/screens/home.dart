adc872ad8391d9848c8ecd4ebe2042aa9842fc54
Uchencho <aloziekelechi17@gmail.com>
2021-04-11 16:28:58 +0100

    hover and refactored custom icon

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -16,68 +16,88 @@ class Home extends StatelessWidget {
         ),
       ),
       child: Scaffold(
-          backgroundColor: Colors.transparent,
-          bottomNavigationBar: Container(
-            padding: EdgeInsets.only(bottom: 15.0),
-            child: Row(
+        backgroundColor: Colors.transparent,
+        bottomNavigationBar: Container(
+          padding: EdgeInsets.only(bottom: 15.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                FontAwesomeIcons.copyright,
+                size: kFooterIconTextSize,
+                color: Colors.white,
+              ),
+              Text(
+                ' Uchencho',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: kFooterIconTextSize,
+                    color: Colors.white,
+                    fontWeight: FontWeight.w600),
+              ),
+            ],
+          ),
+        ),
+        body: Container(
+          alignment: Alignment.center,
+          child: Container(
+            padding: EdgeInsets.only(left: 30.0),
+            child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(
-                  FontAwesomeIcons.copyright,
-                  size: kFooterIconTextSize,
-                  color: Colors.white,
+                Row(
+                  children: [
+                    Text("Uchenna", style: kUcheTextStyle),
+                    Expanded(child: Text(" Alozie", style: kAlozieTextStyle))
+                  ],
                 ),
-                Text(
-                  ' Uchencho',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: kFooterIconTextSize,
-                      color: Colors.white,
-                      fontWeight: FontWeight.w600),
-                ),
+                Container(
+                    color: Colors.grey.shade800,
+                    width: 5000,
+                    margin: EdgeInsets.only(right: 30.0),
+                    child: Text(kDescription, style: kRoleDescription)),
+                SizedBox(height: 20),
+                Container(
+                  child: Row(
+                    children: [
+                      CustomIcon(
+                          ic: FontAwesomeIcons.twitter,
+                          hoverColor: Colors.blue),
+                      CustomIcon(
+                          ic: FontAwesomeIcons.linkedin,
+                          hoverColor: Colors.blue),
+                      CustomIcon(
+                          ic: FontAwesomeIcons.github,
+                          hoverColor: Colors.black),
+                      CustomIcon(
+                          ic: FontAwesomeIcons.envelope,
+                          hoverColor: Colors.red),
+                    ],
+                  ),
+                )
               ],
             ),
           ),
-          body: Container(
-            alignment: Alignment.center,
-            child: Container(
-              padding: EdgeInsets.only(left: 30.0),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text("Uchenna", style: kUcheTextStyle),
-                      Expanded(child: Text(" Alozie", style: kAlozieTextStyle))
-                    ],
-                  ),
-                  Container(
-                      color: Colors.grey.shade800,
-                      width: 5000,
-                      margin: EdgeInsets.only(right: 30.0),
-                      child: Text(kDescription, style: kRoleDescription)),
-                  SizedBox(height: 20),
-                  Container(
-                    width: 140,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Icon(FontAwesomeIcons.twitter,
-                            color: Colors.white, size: kIconSize),
-                        Icon(FontAwesomeIcons.linkedin,
-                            color: Colors.white, size: kIconSize),
-                        Icon(FontAwesomeIcons.github,
-                            color: Colors.white, size: kIconSize),
-                        Icon(FontAwesomeIcons.envelope,
-                            color: Colors.white, size: kIconSize)
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomIcon extends StatelessWidget {
+  CustomIcon({@required this.ic, @required this.hoverColor});
+  final IconData ic;
+  final Color hoverColor;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+      style: ButtonStyle(
+        overlayColor: MaterialStateProperty.all<Color>(this.hoverColor),
+      ),
+      onPressed: () {},
+      child: Icon(this.ic, color: Colors.white, size: kIconSize),
     );
   }
 }
