ea98b62a47873962096a6cfe279ada66af2190c2
Uchencho <aloziekelechi17@gmail.com>
2021-04-11 14:51:01 +0100

    Added paicifico font and footer

--- lib/screens/const.dart
+++ lib/screens/const.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
-const kUcheTextStyle =
-    TextStyle(color: Colors.white, fontSize: 80, fontWeight: FontWeight.bold);
+const kUcheTextStyle = TextStyle(
+    color: Colors.white,
+    fontSize: 90,
+    fontWeight: FontWeight.bold,
+    fontFamily: "Pacifico");
 
-const kAlozieTextStyle =
-    TextStyle(color: Colors.yellow, fontSize: 80, fontWeight: FontWeight.bold);
+const kAlozieTextStyle = TextStyle(
+    color: Colors.yellow,
+    fontSize: 90,
+    fontWeight: FontWeight.bold,
+    fontFamily: "Pacifico");
 
 const kRoleDescription =
     TextStyle(color: Colors.white, fontSize: 30, fontWeight: FontWeight.w300);
 
 const kIconSize = 25.0;
+const kFooterIconTextSize = 15.0;
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -5,53 +5,75 @@ import 'package:portfolio/screens/const.dart';
 class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        alignment: Alignment.center,
-        child: Container(
-          padding: EdgeInsets.only(left: 30.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                children: [
-                  Text("Uchenna", style: kUcheTextStyle),
-                  Expanded(child: Text(" Alozie", style: kAlozieTextStyle))
-                ],
-              ),
-              SizedBox(height: 10),
-              Text("Software craftman", style: kRoleDescription),
-              SizedBox(height: 20),
-              Container(
-                width: 140,
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Icon(FontAwesomeIcons.twitter,
-                        color: Colors.white, size: kIconSize),
-                    Icon(FontAwesomeIcons.linkedin,
-                        color: Colors.white, size: kIconSize),
-                    Icon(FontAwesomeIcons.github,
-                        color: Colors.white, size: kIconSize),
-                    Icon(FontAwesomeIcons.envelope,
-                        color: Colors.white, size: kIconSize)
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
-        decoration: BoxDecoration(
-          color: Color(0xFF686868),
-          image: DecorationImage(
-            image: AssetImage("images/background.jpg"),
-            fit: BoxFit.cover,
-            colorFilter: ColorFilter.mode(
-                Colors.black.withOpacity(0.2), BlendMode.dstATop),
-          ),
+    return Container(
+      decoration: BoxDecoration(
+        color: Color(0xFF686868),
+        image: DecorationImage(
+          image: AssetImage("images/background.jpg"),
+          fit: BoxFit.cover,
+          colorFilter: ColorFilter.mode(
+              Colors.black.withOpacity(0.2), BlendMode.dstATop),
         ),
       ),
+      child: Scaffold(
+          backgroundColor: Colors.transparent,
+          bottomNavigationBar: Container(
+            padding: EdgeInsets.only(bottom: 15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(
+                  FontAwesomeIcons.copyright,
+                  size: kFooterIconTextSize,
+                  color: Colors.white,
+                ),
+                Text(
+                  ' Uchencho',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                      fontSize: kFooterIconTextSize,
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600),
+                ),
+              ],
+            ),
+          ),
+          body: Container(
+            alignment: Alignment.center,
+            child: Container(
+              padding: EdgeInsets.only(left: 30.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text("Uchenna", style: kUcheTextStyle),
+                      Expanded(child: Text(" Alozie", style: kAlozieTextStyle))
+                    ],
+                  ),
+                  Text("Software craftman", style: kRoleDescription),
+                  SizedBox(height: 20),
+                  Container(
+                    width: 140,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Icon(FontAwesomeIcons.twitter,
+                            color: Colors.white, size: kIconSize),
+                        Icon(FontAwesomeIcons.linkedin,
+                            color: Colors.white, size: kIconSize),
+                        Icon(FontAwesomeIcons.github,
+                            color: Colors.white, size: kIconSize),
+                        Icon(FontAwesomeIcons.envelope,
+                            color: Colors.white, size: kIconSize)
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )),
     );
   }
 }
